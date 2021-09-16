2d966bd45eb3480752c018c7a104e6ab6ac56c08
stalin54 <88694817+stalin54@users.noreply.github.com>
2021-09-16 18:18:14 -0500

    Int2
    carga documentos intweb

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "INT2";
+
+// Site URL
+$site_url = "http://localhost/INT2/facebook/social/Script/"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "1234456789"; // Your purchase code, don't give it to anyone. 
+?>
